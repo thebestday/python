2e9ec721629ec0da0d89a6c51bc47acc5c78c7d8
For Web <bestweb@europe.com>
2019-12-14 19:56:58 +0500

    file README

---
+++ L2/type_var.py
@@ -0,0 +1,14 @@
+# Типы переменных
+# Марка
+name = 'ford'
+print(name, type(name)) # class string
+# Age
+age = 3
+print(age, type(age)) # class int
+# Обьем двигателя
+engine_volume = 1.6
+print(engine_volume, type(engine_volume)) # class float
+# Есть ли люк
+see_sky = False
+print(see_sky, type(see_sky)) # class bool
+
