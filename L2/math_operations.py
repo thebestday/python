c9885ec859ca94c057b6f070204cafa9d554bdd4
For Web <bestweb@europe.com>
2019-12-18 20:09:35 +0500

    math_operation

---
+++ L2/math_operations.py
@@ -0,0 +1,32 @@
+'''
+a = input('First number')
+b = input('Second number')
+a = int(a)
+b = int(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+result = a / b
+print(type(result))
+print(result)
+print(a ** 2)
+
+'''
+a = True
+b = False
+# Логические операции
+# Отрицание
+print(not a)
+# ЛОГИЧЕСКОЕ И
+print(a and b)
+# ЛОГИЧЕСКОЕ ИЛИ
+print(a or b)
+print('------')
+a = 10
+print(a > 100)
+print(a < 100)
+print(a <= 100)
+print(a >= 100)
+print(a == 100)
+print(a != 100)
